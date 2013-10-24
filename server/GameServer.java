e8f5ec2dc7dc7fed5e8b6835579cdb4f0de88480
jaco3319 <jacobpoulsen123@gmail.com>
2013-10-24 11:17:38 +0200

    GameServer coded

--- server/GameServer.java
+++ server/GameServer.java
@@ -2,19 +2,55 @@ package server;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class GameServer {
 
-	private int portNumber;
-	private ServerSocket serverSocket;
-	private Socket connection;
-	private static final int maxClientsCount = 10;
-	private static final ClientThread[] threads = new ClientThread[maxClientsCount];
+	private static Socket connection;
+	private static ServerSocket serverSocket;
+	private final static int maxClientsCount = 10;
+	private final static ClientThread[] threads = new ClientThread[maxClientsCount];
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-}
+		// Default port number
+		int portNumber = 90210;
+		// if you want to make your own portNumber
+		if (args.length < 1) {
+		      System.out
+		          .println("Usage: java MultiThreadChatServer <portNumber>\n"
+		              + "Now using port number=" + portNumber);
+		    } else {
+		      portNumber = Integer.valueOf(args[0]).intValue();
+		    }
+		//Open server socket on portNumber
+		try{
+			serverSocket = new ServerSocket(portNumber);
+		}catch(IOException ioException){
+			ioException.printStackTrace();
+		}
+		
+		//Wait for clients to connect
+		while (true) {
+		      try {
+		        connection = serverSocket.accept();
+		        int i = 0;
+		        for (i = 0; i < maxClientsCount; i++) {
+		          if (threads[i] == null) {
+		            (threads[i] = new ClientThread(connection, threads)).start();
+		            break;
+		          }
+		        }
+		        if (i == maxClientsCount) {
+		        	DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+		        	out.writeBytes("Too many people on the server, please try again later \n");
+		        	out.close();
+		        	connection.close();
+		        }
+		      } catch (IOException e) {
+		        System.out.println(e);
+		      }
+		    }
+		  }
+		}
