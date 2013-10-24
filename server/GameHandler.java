e492129ee62683c49c184d1c11fa39ba1931fd7d
jnyborg <jnyborg@outlook.com>
2013-10-24 12:24:48 +0200

    hi there

---
+++ server/GameHandler.java
@@ -0,0 +1,17 @@
+package server;
+
+import game.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameHandler {
+	private List<Player> players;
+	
+	public GameHandler() {
+		players = new ArrayList<Player>();
+	}
+	
+	
+	
+}
